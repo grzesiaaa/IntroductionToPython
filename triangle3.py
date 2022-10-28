3341b20fadb5c625daba49424e842388ae4053e9
grzesiaaa <81617044+grzesiaaa@users.noreply.github.com>
2022-10-28 18:42:23 +0200

    Delete triangle3.py

--- triangle3.py
+++
@@ -1,8 +0,0 @@
-def triangle(start, end, symbol1, symbol2):
-    for i in range(start, end,-2):
-        if (i%4 == 1): 
-            print(" " * int(0.5*(start-i)) + i*symbol2)
-        if (i%4 == 3):
-          print(" " * int(0.5*(start-i)) + i*symbol1)
-   
-print(triangle(7,0,'*', '+')) 
