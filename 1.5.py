e7c804432fad02df980324d2033a0f04e3d46534
grzesiaaa <81617044+grzesiaaa@users.noreply.github.com>
2022-10-28 18:41:12 +0200

    Delete 1.5.py

--- 1.5.py
+++
@@ -1,7 +0,0 @@
-import datetime
-
-run = datetime.timedelta(hours = 6, minutes = 52)
-recreation = datetime.timedelta(minutes = 6, seconds = 15)
-fast = datetime.timedelta(minutes = 4, seconds = 12)
-
-print(run + 1.5*recreation + 4.8*fast + 1*recreation)
